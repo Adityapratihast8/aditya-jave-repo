e217edd10046e16bdf7236131a65d800a5f500a2
Adityapratihast8 <aadityapratihast8@gmail.com>
2025-05-02 22:36:52 +0530

    halfpiramid

---
+++ invertedtriangel.java
@@ -0,0 +1,9 @@
+public class invertedtriangel {
+    public static void main(String[] args) {
+        for(int line =  1 ;  line <= 5 ; line++){
+            for(int star = 1 ;  star <=  line; star++){
+                System.out.print("*");
+            }System.out.println();
+        }//System.out.println();
+    }
+}
